73a49c9719894408c8886ee15f2215fcf32e874e
Flora <Flora_4er@163.com>
2020-09-27 16:46:54 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 手动配置element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
+
+//导入全局样式表
+import './assets/css/global.css'
+
+import axios from 'axios'
+//配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
